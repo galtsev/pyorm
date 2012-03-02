7d7f0e619ffff9f0e7cb736a8c9da49cef7d41ae
Denis Galtsev <galtsev@logicalware.com>
2012-03-02 12:02:02 +0000

    prepare for distutils

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+        name = 'pyorm',
+        version = '0.2',
+        description = 'Object-relational mapper',
+        author = 'Denis Galtsev',
+        author_email = 'galtsev@logicalware.com',
+        packages = ['pyorm'],
+        package_dir = {'': 'src'}
+     )
+
